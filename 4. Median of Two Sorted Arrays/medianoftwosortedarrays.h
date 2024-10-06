39aeb5734b2755cb6830c3fc96df20b761580526
Burak Şen <buraksen7@hotmail.com>
2024-10-06 15:57:31 +0200

    Leetcode Problem 4

--- 4. Median of Two Sorted Arrays/medianoftwosortedarrays.h
+++ 4. Median of Two Sorted Arrays/medianoftwosortedarrays.h
@@ -1,28 +1,47 @@
 #pragma once
-
-#include <stdlib.h>
+#include <limits.h>
+#include <math.h>
 
 double findMedianSortedArrays(int *nums1, int nums1Size, int *nums2, int nums2Size)
 {
-    int totalSize = nums1Size + nums2Size;
-    int *merged = (int *)malloc(sizeof(int) * totalSize);
-    int i = 0, j = 0, k = 0;
-    while (i < nums1Size && j < nums2Size)
+    if (nums1Size > nums2Size)
     {
-        if (nums1[i] < nums2[j])
-            merged[k++] = nums1[i++];
-        else
-            merged[k++] = nums2[j++];
+        return findMedianSortedArrays(nums2, nums2Size, nums1, nums1Size);
     }
-    while (i < nums1Size)
-        merged[k++] = nums1[i++];
-    while (j < nums2Size)
-        merged[k++] = nums2[j++];
-    double median = 0;
-    if (totalSize % 2 == 0)
-        median = (merged[totalSize / 2 - 1] + merged[totalSize / 2]) / 2.0;
-    else
-        median = merged[totalSize / 2];
-    free(merged);
-    return median;
+
+    int low = 0, high = nums1Size;
+    while (low <= high)
+    {
+        int partitionX = (low + high) / 2;
+        int partitionY = (nums1Size + nums2Size + 1) / 2 - partitionX;
+
+        int maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+        int minRightX = (partitionX == nums1Size) ? INT_MAX : nums1[partitionX];
+
+        int maxLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+        int minRightY = (partitionY == nums2Size) ? INT_MAX : nums2[partitionY];
+
+        if (maxLeftX <= minRightY && maxLeftY <= minRightX)
+        {
+            if ((nums1Size + nums2Size) % 2 == 0)
+            {
+                return (double)(fmax(maxLeftX, maxLeftY) + fmin(minRightX, minRightY)) / 2.0;
+            }
+            else
+            {
+                return (double)fmax(maxLeftX, maxLeftY);
+            }
+        }
+        else if (maxLeftX > minRightY)
+        {
+            high = partitionX - 1;
+        }
+        else
+        {
+            low = partitionX + 1;
+        }
+    }
+
+    // This should never be reached if the input arrays are sorted
+    return 0.0;
 }
